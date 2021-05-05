0736179ebc92b9705e78caf859b3ecad11686b28
sunggookang <rkdtjdrn90@gmail.com>
2021-05-05 14:32:53 +0900

    antd 기본 적용

--- src/main/index.js
+++ src/main/index.js
@@ -16,41 +16,33 @@ function MainPage() {
         }, [])
     return (
         <div>
-            <div id="header">
-                <div id="header-area">
-                    <img src="images/icons/logo.png" alt="로고 이미지" />
-                </div>
+            <div id="banner">
+                <img src="images/banners/banner1.png" alt="배너 이미지" />
             </div>
-            <div id="body">
-                <div id="banner">
-                    <img src="images/banners/banner1.png" alt="배너 이미지" />
-                </div>
-                <h1>판매되는 상품들</h1>
-                <div id="product-list">
-                    {
-                        products.map(function (product, index) {
-                            return (
-                                <div className="product-card">
-                                    <Link className="product-link" to={`/products/${index}`}>
-                                        <div>
-                                            <img className="product-img" src={product.imageUrl} alt="상품 이미지" />
+            <h1>판매되는 상품들</h1>
+            <div id="product-list">
+                {
+                    products.map(function (product, index) {
+                        return (
+                            <div className="product-card">
+                                <Link style={{ color: "inherit" }} className="product-link" to={`/products/${product.id}`}>
+                                    <div>
+                                        <img className="product-img" src={product.imageUrl} alt="상품 이미지" />
+                                    </div>
+                                    <div className="product-contents">
+                                        <span className="product-name">{product.name}</span>
+                                        <span className="product-price">{product.price}원</span>
+                                        <div className="product-seller">
+                                            <img className="product-avatar" src="images/icons/avatar.png" alt="아바타 이미지" />
+                                            <span>{product.seller}</span>
                                         </div>
-                                        <div className="product-contents">
-                                            <span className="product-name">{product.name}</span>
-                                            <span className="product-price">{product.price}원</span>
-                                            <div className="product-seller">
-                                                <img className="product-avatar" src="images/icons/avatar.png" alt="아바타 이미지" />
-                                                <span>{product.seller}</span>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
             </div>
-            <div id="footer"></div>
         </div>
     );
 }
